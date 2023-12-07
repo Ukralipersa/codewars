c16f08134b3540a231a6e9030e6ed1e59b3730e0
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2023-12-07 18:55:56 +0300

    you-only-need-one-beginner-4

--- 8kyu/you-only-need-one-beginner.js
+++ 8kyu/you-only-need-one-beginner.js
@@ -4,3 +4,8 @@
 // Array can contain numbers or strings. X can be either.
 
 // Return true if the array contains the value, false if not.
+
+//SOLUTION.
+function check(a, x) {
+  return a.includes(x);
+}
