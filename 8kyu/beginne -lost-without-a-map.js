d13876c718d2f1f380c88e4d3806e4723aa88433
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2023-12-09 20:12:44 +0300

    beginne -lost-without-a-map-3

--- 8kyu/beginne -lost-without-a-map.js
+++ 8kyu/beginne -lost-without-a-map.js
@@ -1,6 +1,6 @@
-DESCRIPTION:
-Given an array of integers, return a new array with each value doubled.
+// DESCRIPTION:
+// Given an array of integers, return a new array with each value doubled.
 
-For example:
+// For example:
 
-[1, 2, 3] --> [2, 4, 6]
+// [1, 2, 3] --> [2, 4, 6]
