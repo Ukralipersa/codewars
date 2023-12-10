a05a0fac6d0c7b950703816ed942322bd656581c
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2023-12-10 20:38:07 +0300

    sum-of-differences-in-array-4

--- 8kyu/sum-of-differences-in-array.js
+++ 8kyu/sum-of-differences-in-array.js
@@ -1,2 +1,10 @@
 // DESCRIPTION:
 // Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+//SOLUTION:
+function sumOfDifferences(arr) {
+  return arr
+    .sort((a, b) => b - a)
+    .map((val, i, arr) => (i === arr.length - 1 ? 0 : val - arr[i + 1]))
+    .reduce((acc, val) => acc + val, 0);
+}
