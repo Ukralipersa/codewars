0fb0f398af79a92ead14c47ad821bd122e288f8d
Ukralipersa <149258019+Ukralipersa@users.noreply.github.com>
2023-12-11 20:43:14 +0300

    did-she-say-hallo-4

--- 8kyu/did-she-say-hallo.js
+++ 8kyu/did-she-say-hallo.js
@@ -17,3 +17,14 @@
 // you can assume the input is a string.
 // to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
 // function should be case insensitive to pass the tests
+
+//SOLUTION:
+function validateHello(greetings) {
+  const words = ["hello", "hola", "ciao", "salut", "hallo", "ahoj", "czesc"];
+  for (let i = 0; i < words.length; i++) {
+    if (greetings.toLowerCase().includes(words[i])) {
+      return true;
+    }
+  }
+  return false;
+}
